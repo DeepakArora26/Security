933827eb2b224c9fb3751806b5338e4ff5355eee
Deepak_Arora <Deepakarora0145@gmail.com>
2021-06-13 15:43:25 +0530

    Added /contactUs as the exception in the SecurityConfig

--- src/main/java/com/masco/security/configuration/SecurityConfig.java
+++ src/main/java/com/masco/security/configuration/SecurityConfig.java
@@ -9,6 +9,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/contactUs").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
