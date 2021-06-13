bdcb6fd300fda7e80b9e83a243a5b32a07048d05
Deepak_Arora <Deepakarora0145@gmail.com>
2021-06-13 14:39:28 +0530

    Created /login API and made changes in credentials.

---
+++ src/main/java/com/masco/security/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.masco.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getHelloMessage() {
+        return "Hello World";
+    }
+}
